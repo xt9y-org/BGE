8cc407adea8232d13d032176aca650229ee107a7
xt9y <felix@xt9y.de>
2026-09-08 23:30:06 +0200

    refactor: define clean application api

--- Engine/App.h
+++ Engine/App.h
@@ -1,45 +1,20 @@
-#pragma once
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
+#ifndef BGE_APP_H
+#define BGE_APP_H
 
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "util/types.h"
 
-// Engine Application API
-// Simple interface for creating OpenGL applications
-
-// Main macro
-// and the engine handles the rest
-void INPUT();
-void RENDER();
-void RUN();
-#define ENGINE_ENTRY_POINT \
-    int main() { \
-        RUN(); \
-        return 0; \
-    }
-
-#define TITLE  "opengl _f"
-#define WIDTH 1270 
+#define TITLE "opengl _f"
+#define WIDTH 1270
 #define HEIGHT 800
 #define RENDER_BASE_W 200
 #define PALETTE_LEVELS 32.0f
 
-void GL_START();
-int  GL_FRAME();
-void GL_END();
-double GL_GETFPS();
-
-#include "Engine/gfx.h"
-#include "Engine/state.h"
-#include "Engine/util/math.h"
-#include "Engine/util.h"
-
+int app_run(void);
+double app_get_fps(void);
+void app_request_exit(void);
 
 extern u32 g_fbo;
-void fbo_resize(const i32 w, const i32 h);
+void fbo_resize(i32 w, i32 h);
 void post_blit(i32 src_w, i32 src_h, i32 dst_w, i32 dst_h);
 
+#endif
