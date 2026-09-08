76e68158c35bb716f1cf2842021c9a3876c1f699
xt9y <felix@xt9y.de>
2026-09-08 23:29:07 +0200

    refactor: remove callback camera dependency

--- Engine/cam.c
+++ Engine/cam.c
@@ -1,6 +1,6 @@
 #include "cam.h"
 #include "util/math.h"
-#include <glad/glad.h>
+
 #include <math.h>
 
 void update_camera_vectors(camera_t* cam)
@@ -12,22 +12,12 @@ void update_camera_vectors(camera_t* cam)
     cam->front = vec3_normalize(front);
 }
 
-void camera_mouse_callback(camera_t* cam, const f64 xpos, const f64 ypos)
+void camera_apply_mouse_delta(camera_t* cam, const f32 dx, const f32 dy)
 {
     if (!cam) return;
-    if (cam->firstMouse) {
-        cam->lastX = (f32)xpos;
-        cam->lastY = (f32)ypos;
-        cam->firstMouse = false;
-    }
 
-    const f32 xoffset = ((f32)xpos - cam->lastX) * 0.1f;
-    const f32 yoffset = (cam->lastY - (f32)ypos) * 0.1f;
-    cam->lastX = (f32)xpos;
-    cam->lastY = (f32)ypos;
-
-    cam->yaw   += xoffset;
-    cam->pitch += yoffset;
+    cam->yaw += dx * 0.1f;
+    cam->pitch += dy * 0.1f;
 
     if (cam->pitch > 89.0f) cam->pitch = 89.0f;
     if (cam->pitch < -89.0f) cam->pitch = -89.0f;
