0034535d9be1efdc58a1c21cdb0647bc186d3bcf
xt9y <felix@xt9y.de>
2026-09-08 23:37:49 +0200

    refactor: add imgui lwcgl backend interface

---
+++ Engine/imgui_impl_lwcgl.h
@@ -0,0 +1,12 @@
+#ifndef IMGUI_IMPL_LWCGL_H
+#define IMGUI_IMPL_LWCGL_H
+
+bool ImGui_ImplLwcgl_Init(void);
+void ImGui_ImplLwcgl_Shutdown(void);
+void ImGui_ImplLwcgl_NewFrame(float dt,
+                              int window_w,
+                              int window_h,
+                              int framebuffer_w,
+                              int framebuffer_h);
+
+#endif
